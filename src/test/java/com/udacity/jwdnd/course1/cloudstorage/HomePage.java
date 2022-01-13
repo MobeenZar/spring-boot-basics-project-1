e6a2d874e738344eb9604317fec11620680c3ddf
Mobeen Zar <mobeen_zar@hotmail.com>
2022-01-13 09:53:54 +0300

    Testing

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage {
+public class HomePage { 
     @FindBy(id = "btnLogout")
     private WebElement logoutButton;
 
