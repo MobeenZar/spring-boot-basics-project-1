9c3115db33a2c55e5733d4dcef797b0f9f667336
Mobeen Zar <mobeen_zar@hotmail.com>
2022-01-13 15:37:51 +0300

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-Select * from users;
---INSERT INTO USERS (userid, username, salt, password, firstname, lastname) VALUES(10, 'user','salt', 'user', 'Mobeen', 'Zar');
---INSERT INTO USERS (userid, username, salt, password, firstname, lastname) VALUES(20, 'admin','salt', 'admin', 'Naba', 'Zar');
---
---INSERT INTO NOTES (noteid, notetitle, notedescription, userid) VALUES (10,'Title Mobeen', 'hello world', 10 );
---INSERT INTO NOTES (noteid, notetitle, notedescription, userid) VALUES (20,'Title Mobeen 2', 'hello world 2', 10 );
-
---INSERT INTO CREDENTIALS (credentialid, url, username, key, password, userid)  VALUES(1, 'my url', 'user', 'my key', 'my pass', 10);
