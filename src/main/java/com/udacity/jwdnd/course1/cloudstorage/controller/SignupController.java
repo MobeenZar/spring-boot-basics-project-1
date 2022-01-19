3e88bbfabffa2e447c3d3a2dabeb8988d4dde7bc
Mobeen Zar <mobeen_zar@hotmail.com>
2022-01-19 18:16:34 +0300

    login redirect

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,8 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
-
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
-import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,6 +45,6 @@ public class SignupController {
             model.addAttribute("signupError", signupError);
         }
 
-        return "signup";
+        return "login";
     }
 }
